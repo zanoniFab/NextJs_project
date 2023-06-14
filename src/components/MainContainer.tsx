bc220e2d59d6d6478892d1083b6d0f0472d1d6e9
zanoniFab <fabianezanoni@gmail.com>
2023-06-14 16:47:09 -0300

    Altera tsconfig para build

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,7 +1,13 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
+import {ReactNode} from "react";
 
-export default function MainContainer({children}) {
+interface Props {
+    children: ReactNode
+}
+
+export default function MainContainer({children}: Props) {
+
     return (
         <>
             <Navbar/>
