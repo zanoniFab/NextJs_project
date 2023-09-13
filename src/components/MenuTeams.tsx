d95809f01bdb7eaaf843b5836b9790fb38d7a784
zanoniFab <fabianezanoni@gmail.com>
2023-09-13 15:00:09 -0300

    cria componente e altera estrutura da page times

---
+++ src/components/MenuTeams.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import styles from "../styles/Times.module.css"
+export default function MenuTeams({listaTimes}) {
+
+    return (
+        <div className={styles.boxList}>
+        <h3>Times disponíveis para consulta:</h3>
+
+        <div className={styles.listContainer}>
+            <ul className={styles.teamList}>
+                {listaTimes.map((clube)=>
+                    <li key={clube.id} >
+                        <img
+                        src = {clube.escudos["60x60"]} 
+                        width="10"
+                        height="10"
+                        alt="logo time"
+                        />
+                        <Link href={`/times/${clube.id}`}>{clube.nome}</Link>
+                    </li>
+                )}
+            </ul>
+        </div>
+    </div>
+    )
+}
